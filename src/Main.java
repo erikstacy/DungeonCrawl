d3eb75ceb7477f0237a41c530b3358a715d80733
erikstacy <erikslaststand@gmail.com>
2019-06-16 10:33:34 -0500

    Added java

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        printIntro();
+    }
+
+
+
+    public static void printIntro() {
+        System.out.println("Welcome to D U N G E O N    C R A W L");
+    }
+
+}
